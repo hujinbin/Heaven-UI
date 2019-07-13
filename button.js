97af9e6119fe844143635c80b0719b968063336c
hujinbin <437044048@qq.com>
2019-07-13 17:11:51 +0800

    edit

--- button.js
+++
@@ -1,3 +0,0 @@
-Vue.component('h-buttom',{
-    template:'<button class="h-button">默认</button>'
-})
--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+import Vue from 'vue'
+import Button from './button'
+
+Vue.component('h-button',Button)
+
 new Vue({
   el:'#app',
 })
