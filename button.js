0d437b290a43ee7b7ca7fbe33aaabfb1348e3b4b
hujinbin <437044048@qq.com>
2019-07-13 11:32:35 +0800

    edit

---
+++ button.js
@@ -0,0 +1,3 @@
+Vue.component('h-buttom',{
+    template:'<button class="h-button">默认</button>'
+})
---
+++ src/button.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    <button class="h-button">默认</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'button',
+  data() {
+    return {}
+  },
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
