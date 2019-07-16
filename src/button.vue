5cb49c5549f463b0c311ce590f35971b4aae7b56
hujinbin <437044048@qq.com>
2019-07-16 15:22:10 +0800

    edit

--- src/button.vue
+++ src/button.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="">
-    <button class="h-button">默认</button>
-  </div>
+    <button class="h-button">
+      <span v-if="icon" :class="icon"></span>
+      <slot></slot>
+    </button>
 </template>
 
 <script>
 export default {
   name: 'button',
+  props:['icon'],
   data() {
     return {}
   },
