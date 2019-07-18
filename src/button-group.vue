9c607fea06379793ecf477039711823713a59433
hujinbin <437044048@qq.com>
2019-07-18 18:25:28 +0800

    edit

--- src/button-group.vue
+++ src/button-group.vue
@@ -12,10 +12,20 @@ export default {
   data() {
     return {}
   },
+  mounted(){
+    for (let node of this.$el.children){
+      if((node.nodeName).toLowerCase!=='button'){
+        console.warn('插入异常')
+      }
+    }
+  },
   methods: {},
 }
 </script>
 
 <style lang="scss" scoped>
-
+   .h-button-group{
+      display: inline-flex;
+      vertical-align: middle;
+   }
 </style>
