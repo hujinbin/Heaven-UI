4faa4438455a3bc38a90a7dbfb9d7599611a2982
hujinbin <437044048@qq.com>
2019-07-20 16:22:55 +0800

    edit

---
+++ index.js
@@ -0,0 +1,6 @@
+import Button from './src/button'
+import Icon from './src/icon'
+import ButtonGroup from './src/button-group'
+
+
+export {Button,Icon,ButtonGroup}
--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('h-button',Button)
 Vue.component('h-icon',Icon)
+Vue.component('h-button-group',ButtonGroup)
 
 new Vue({
   el:'#app',
