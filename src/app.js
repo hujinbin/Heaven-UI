387f9b1312a1463302b3cfdb2a3c740b49b6fc69
hujinbin <437044048@qq.com>
2019-07-22 18:26:33 +0800

    edit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import Input from './input'
 
 Vue.component('h-button',Button)
 Vue.component('h-icon',Icon)
 Vue.component('h-button-group',ButtonGroup)
+Vue.component('h-input',Input)
 
 new Vue({
   el:'#app',
---
+++ src/input.vue
@@ -0,0 +1,21 @@
+<template>
+    <span class="h-input">
+    </span>
+</template>
+
+<script>
+export default {
+  name: 'icon',
+  props:{
+    name:String,
+  },
+  data() {
+    return {}
+  },
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
