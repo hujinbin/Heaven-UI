8a194b4d179745a0cbd8fac7fa5b361d72881aa1
hujinbin <437044048@qq.com>
2019-08-09 18:20:16 +0800

    edit

--- src/icon.vue
+++ src/icon.vue
@@ -1,6 +1,5 @@
 <template>
-    <span class="h-icon">
-    </span>
+    <i class="h-icon" :class="`h-icon-${name}`"></i>
 </template>
 
 <script>
@@ -17,5 +16,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  @import '//at.alicdn.com/t/font_1343551_0qtdnpb2xpoi.css'; 
+  .h-icon{
+    font-style: normal;
+  }
 </style>
