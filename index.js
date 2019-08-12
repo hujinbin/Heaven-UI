32cce6832a165daafa191b09bc3a66d8e5abf770
hujinbin <437044048@qq.com>
2019-08-12 18:42:52 +0800

    edit

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ import ButtonGroup from './src/button-group'
 import Input from './input'
 
 
-export {Button,Icon,ButtonGroup,Input}
+const install = Vue => {
+    Vue.component(Button.name, Button)
+    Vue.component(Icon.name, Icon)
+    Vue.component(ButtonGroup.name, ButtonGroup)
+    Vue.component(Input.name, Input)
+}
+export default install
+
+// export {Button,Icon,ButtonGroup,Input}
