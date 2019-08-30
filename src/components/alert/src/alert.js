fe9c449c52eccef001412d2eeca76b5fa4a1c4cd
hujinbin <437044048@qq.com>
2019-08-30 18:22:55 +0800

    alert组件修改

--- src/components/alert/src/alert.js
+++ src/components/alert/src/alert.js
@@ -4,7 +4,11 @@ import msg from './msg.vue';
 
 
 const Alert = function(options) {
-
+    const Alert=Vue.extend(alert)
+    const alertComponent = new Alert({el: document.createElement('div')});
+    alertComponent.text = options.text;
+    // 把alert加入body中
+    alertComponent.$appendTo(document.body);
 }
 
 export default Alert;
