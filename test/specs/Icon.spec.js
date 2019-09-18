99d24997dd0a6308dee5bf05f7db5c129982a280
hujinbin <437044048@qq.com>
2019-09-18 18:36:04 +0800

    edit

--- test/specs/Icon.spec.js
+++ test/specs/Icon.spec.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Icon from '@/components/icon'
 
-describe('Hello.vue', () => {
-  it('should render correct contents', () => {
+describe('icom.vue', () => {
+  it('name', () => {
     const Constructor = Vue.extend(Icon)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
