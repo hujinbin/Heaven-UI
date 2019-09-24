01621dfae891ac656ed35df7725c9aba32519b83
hujinbin <437044048@qq.com>
2019-09-24 18:21:04 +0800

    add

---
+++ src/components/checkbox/src/checkbox.vue
@@ -0,0 +1,26 @@
+<template>
+   <label class="h-checkbox">
+        <span class="h-checkbox-input">
+           <span class="h-radio-inner"></span>
+           <input type="checkbox" class="h-checkbox-original" :value="value" @change="$emit('change',$event.target.value)">
+        </span>
+       <span class="h-checkbox-label">
+           <slot></slot>
+       </span>
+   </label>
+</template>
+
+<script>
+export default {
+ name: 'h-checkbox',
+  props:{
+    value: Number || String,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.h-radio{
+
+}
+</style>
