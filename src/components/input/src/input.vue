b0685e28ee78565633044ff7f195e7d4d4d0f176
hujinbin <437044048@qq.com>
2019-09-30 18:02:15 +0800

    edit

--- src/components/input/src/input.vue
+++ src/components/input/src/input.vue
@@ -3,6 +3,7 @@
       <textarea v-if="type === 'textarea'"
       :value='value'
       :placeholder="placeholder"
+      :style="`height:${rows?rows*30 : 60}`"
       @input="$emit('input',$event.target.value)">
       </textarea>
       <template v-else>
@@ -34,6 +35,9 @@ export default {
       type:Boolean,
       default:false,
     },
+    rows:{ //文本域默认行数，仅在 textarea 类型下有效
+      type:Number,
+    },
     readonly:{ 
       type:Boolean,
       default:false,
@@ -55,8 +59,8 @@ export default {
   display: inline-block;
   position: relative;
   width: 100%;
+  font-size: 14px;
   >input{
-    background: #fff;
     border-radius: 2px;
     border: 1px solid #dcdfe6;
     box-sizing: border-box;
@@ -69,20 +73,24 @@ export default {
     padding: 0 10px;
     width: 100%;
     &:focus{
-      border: 1px solid #78797d;
+      border: 1px solid #0c80f9;
     }
   }
   >textarea{
-    background: #fff;
+    width: 100%;
+    padding: 0 10px;
     border-radius: 2px;
     border: 1px solid #dcdfe6;
     box-sizing: border-box;
     color: #78797d;
     display: inline-block;
     font-size: inherit;
-    min-height: 40px;
-    line-height: 40px;
-    padding: 10px;
+    height: 60px;
+    line-height: 1.5;
+    padding:5px 10px;
+    &:focus{
+      border: 1px solid #0c80f9;
+    }
   }
   .error-text{
     color: red;
