788037e5a883d57de4e024e844ec5c8c867aa845
hujinbin <437044048@qq.com>
2019-10-11 18:25:19 +0800

    edit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import Button from './components/button/index'
 import Icon from './components/icon/index'
 import ButtonGroup from './components/button-group/index'
 import Input from './components/input/index'
+import Radio from './components/radio/index'
 
 const install = Vue => {
     Vue.component(Button.name, Button)
     Vue.component(Icon.name, Icon)
     Vue.component(ButtonGroup.name, ButtonGroup)
     Vue.component(Input.name, Input)
+    Vue.component(Radio.name, Radio)
 
     Vue.prototype.$alert = Alert;
 }
