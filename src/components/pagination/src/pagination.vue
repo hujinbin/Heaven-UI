742dd55d809fd3e8ad3fb3de2c7291606e15352e
hujinbin <437044048@qq.com>
2019-11-11 18:24:22 +0800

    edit

--- src/components/pagination/src/pagination.vue
+++ src/components/pagination/src/pagination.vue
@@ -1,13 +1,27 @@
 <template>
-
+   <ul class="h-pagination">
+      <li 
+         :class="{ active: currentPage === 1, disabled }">
+         </li>
+      <li></li>
+      <li></li>
+      <li></li>
+   </ul>
 </template>
 
 <script>
 export default {
  name: 'h-pagination',
   props:{
+    currentPage: Number,
+
+    pageCount: Number,
+
+    pagerCount: Number,
+
+    disabled: Boolean,
     size:{ 
-      type:Number,
+      type:Number,  //分页大小
     },
   },
     methods: {
