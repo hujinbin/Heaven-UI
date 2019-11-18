9ccc52f70cf95f87938508f307b4de092a5016d1
hujinbin <437044048@qq.com>
2019-11-18 18:18:08 +0800

    edit

--- src/components/pagination/src/pagination.vue
+++ src/components/pagination/src/pagination.vue
@@ -1,17 +1,20 @@
 <template>
    <ul class="h-pagination">
+      <h-icon name="ios-arrow-left"></h-icon>
       <li 
          :class="{ active: currentPage === 1, disabled }">
          </li>
-      <li></li>
-      <li></li>
-      <li></li>
+      <h-icon name="ios-arrow-right"></h-icon>
    </ul>
 </template>
 
 <script>
+import HIcon from '../../icon'
 export default {
  name: 'h-pagination',
+ components:{
+    HIcon
+  },
   props:{
     currentPage: Number,
 
@@ -24,8 +27,31 @@ export default {
       type:Number,  //分页大小
     },
   },
+  data() {
+    return {}
+  },
     methods: {
-
+      emitChange() {
+      this.$nextTick(() => {
+        if (this.internalCurrentPage !== this.lastEmittedPage || this.userChangePageSize) {
+          this.$emit('current-change', this.internalCurrentPage);
+          this.lastEmittedPage = this.internalCurrentPage;
+          this.userChangePageSize = false;
+        }
+      });
+    },
+   //  上一页
+     prev() {
+      if (this.disabled) return;
+      const newVal = this.internalCurrentPage - 1;
+      this.emitChange();
+    },
+   //  下一页
+    next() {
+      if (this.disabled) return;
+      const newVal = this.internalCurrentPage + 1;
+      this.emitChange();
+    },
     }
 }
 </script>
