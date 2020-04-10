399e37ec16b883df91d411f5b6f24a1c62cb861a
hujinbin <437044048@qq.com>
2020-04-10 18:00:54 +0800

    edit

--- src/components/datepicker/src/datepicker.vue
+++ src/components/datepicker/src/datepicker.vue
@@ -1,11 +1,21 @@
 <template>
+   <div class="h-datepicker">
 
+   </div>
 </template>
 
 <script>
-
+export default {
+  name: 'HDatepicker',
+  props:{
+  },
+  data() {
+    return {}
+  },
+  methods: {},
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
