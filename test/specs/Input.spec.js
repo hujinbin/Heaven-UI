e8ed19b4089d3d05e8a494a0a6fa9798dbd59cd1
hujinbin <437044048@qq.com>
2020-04-13 18:08:42 +0800

    edit

--- test/specs/Input.spec.js
+++ test/specs/Input.spec.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Input from '@/components/input'
 
 describe('input.vue', () => {
+  it('input是否存在',()=>{
+    expect(Input).to.be.ok;
+  })
   it('测试name是否生效', () => {
     const Constructor = Vue.extend(Input)
     const vm = new Constructor().$mount()
