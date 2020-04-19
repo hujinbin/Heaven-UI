1f6351ae6c6f61999d6ae7d097a2f466bda576cc
hujinbin <437044048@qq.com>
2020-04-19 20:53:05 +0800

    edit

---
+++ src/components/rate/index.js
@@ -0,0 +1,7 @@
+import HRate from './src/rate';
+
+HRate.install = function(Vue) {
+  Vue.component(HRate.name, HRate);
+};
+
+export default HRate;
--- src/components/rate/src/rate.vue
+++ src/components/rate/src/rate.vue
@@ -1,11 +1,24 @@
 <template>
+  <div class="h-rate">
 
+  </div>
 </template>
 
 <script>
+  export default {
+    name: "HRate",
+    props: {
 
+    },
+    computed: {
+    },
+    methods: {
+    }
+  };
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .h-rate{
+    
+  }
 </style>
