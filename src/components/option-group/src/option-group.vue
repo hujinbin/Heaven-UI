5600424a60cfbbf08693d9024ae3a36f2c77d047
hujinbin <437044048@qq.com>
2020-05-27 17:58:08 +0800

    edit

---
+++ src/components/option-group/src/option-group.vue
@@ -0,0 +1,38 @@
+<template>
+   <div>
+     <solt>  
+     </solt>
+   </div>
+</template>
+
+<script>
+export default {
+  name: "HOptionGroup",
+  props: {
+  },
+  computed: {
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  .h-option{
+    font-size: 14px;
+    padding: 0 20px;
+    position: relative;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    color: #5E5E66;
+    height: 34px;
+    line-height: 34px;
+    box-sizing: border-box;
+    cursor: pointer;
+    &:hover{
+      color: #ffffff;
+      background: #0c80f9;
+    }
+  }
+</style>
