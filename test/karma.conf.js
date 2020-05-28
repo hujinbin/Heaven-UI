96532b9c592377bc4d568e937d651451cfdee15f
hujinbin <437044048@qq.com>
2020-05-28 18:05:23 +0800

    edit

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -2,8 +2,10 @@
 //   http://karma-runner.github.io/0.13/config/configuration-file.html
 // we are also using it with karma-webpack
 //   https://github.com/webpack/karma-webpack
+// const webpack = require('webpack')
 
-var webpackConfig = require('../build/webpack.test.conf')
+
+const webpackConfig = require('../build/webpack.test.conf')
 
 module.exports = function (config) {
   config.set({
@@ -14,9 +16,9 @@ module.exports = function (config) {
     browsers: ['PhantomJS'],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
-    files: ['./index.js'],
+    files: ['/test/specs/*.spec.js'],
     preprocessors: {
-      './index.js': ['webpack', 'sourcemap']
+      '/test/specs/*.spec.js': ['webpack', 'sourcemap']
     },
     webpack: webpackConfig,
     webpackMiddleware: {
---
+++ test/specs/Alert.spec.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Alert from '@/components/Alert'
+
+describe('Alert.vue', () => {
+  it('Alert是否存在',()=>{
+    expect(Alert).to.be.ok;
+  })
+})
