f3768ca0cfba06b7e1c7ed86bca1d27e4f7fdb54
hujinbin <437044048@qq.com>
2020-11-26 17:59:13 +0800

    feat: 表格组件，待继续开发

--- src/components/table-column/src/table-column.vue
+++ src/components/table-column/src/table-column.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class></div>
+  <div></div>
 </template>
 
 <script>
--- src/components/table/src/table.vue
+++ src/components/table/src/table.vue
@@ -1,44 +1,48 @@
 <template>
-   <div class="h-table">
-       <div class="hidden-columns" ref="hiddenColumns">
-          <slot></slot>
-        </div>
-      <div class="h-table-header">
-      </div>
-      <div class="h-table-body">
-          <div
-            v-if="!data || data.length === 0"
+  <div class="h-table">
+    <div class="hidden-columns" ref="hiddenColumns">
+      <slot></slot>
+    </div>
+    <div class="h-table-header"></div>
+    <div class="h-table-body">
+      <div
+        v-if="!data || data.length === 0"
         class="h-table-empty"
-        ref="emptyBlock">
-        <span class="h-table-empty-text" >
+        ref="emptyBlock"
+      >
+        <span class="h-table-empty-text">
           <slot name="empty">{{ emptyText }}</slot>
         </span>
       </div>
-         <slot></slot>
-      </div>
-   </div>
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-   name: 'HTable',
-   components:{
-  
-   },
-   props: {
-     data:Array,
-     emptyText: {
+  name: "HTable",
+  components: {},
+  props: {
+    data: Array,
+    emptyText: {
       type: String,
-      default: '暂无数据'
-     },
-   },
-    methods: {
-    
+      default: "暂无数据",
     },
-}
+  },
+  methods: {},
+};
 </script>
 
 <style lang="scss" scoped>
-  
+.table {
+  position: relative;
+  overflow: hidden;
+  box-sizing: border-box;
+  width: 100%;
+  max-width: 100%;
+  background: #fff;
+  font-size: 14px;
+  color: #1b1c33;
+}
 </style>
